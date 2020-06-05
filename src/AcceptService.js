17aef61835cf068d1259cc3f006be706d2136aa6
WebOsmotic <webosmotic@WebOsmotics-Mac-mini.local>
2020-06-05 16:29:45 +0530

    updating service

--- src/AcceptService.js
+++ src/AcceptService.js
@@ -28,14 +28,14 @@ class AcceptService {
                 url: `${baseUrl}/auth/tokens`,
                 headers: { 'content-type': 'application/json' },
                 data: {
-                    api_key: config.apiKey
+                    api_key: config.apiKey + '123'
                 },
             });
-            console.log('response', response);
             token = response.data.token;
             return true;
         } catch (e) {
-            console.log('getToken error', e)
+            console.warn('error in getting token', e);
+            throw e;
         }
     }
 
@@ -43,17 +43,23 @@ class AcceptService {
         try {
             const orders = await axios({
                 method: 'get',
-                url: `${baseUrl}/ecommerce/orders?token=${token}&merchant_order_id=${options.uniqueId}`,
+                url: `${baseUrl}/ecommerce/orders`,
                 headers: { 'content-type': 'application/json' },
+                params: {
+                    token,
+                    merchant_order_id: options.uniqueId
+                }
             });
-            console.log('orders', orders);
             if (orders.data.results.length) {
                 return { orderId: orders.data.results[0].id };
             }
             const response = await axios({
                 method: 'post',
-                url: `${baseUrl}/ecommerce/orders?token=${token}`,
+                url: `${baseUrl}/ecommerce/orders`,
                 headers: { 'content-type': 'application/json' },
+                params: {
+                    token,
+                },
                 data: {
                     'delivery_needed': 'false',
                     'merchant_id': options.marchantId,
@@ -63,83 +69,92 @@ class AcceptService {
                     'items': [],
                 },
             });
-            console.log('getOrder response===', response);
             return { orderId: response.data.id };
         } catch (e) {
-            console.log('getOrder error', e)
+            console.warn('error in getting order', e)
+            throw e;
         }
     }
 
     async generatePaymentToken(orderId) {
         try {
-            console.log('generatePaymentToken... caled ', token, orderId);
             const response = await axios({
                 method: 'post',
-                url: `${baseUrl}/acceptance/payment_keys?token=${token}`,
+                url: `${baseUrl}/acceptance/payment_keys`,
                 headers: { 'content-type': 'application/json' },
+                params: {
+                    token,
+                },
                 data: {
+                    'auth_token': token,
                     'amount_cents': options.price * 100,
                     'currency': options.currency,
-                    'integration_id': options.type === 'cash' ? config.integration[config.status]['kiosk'] : config.integration[config.status]['card'],
-                    'kiosk_integration_id': options.type === 'cash' ? config.integration[config.status]['kiosk'] : config.integration[config.status]['card'],
-                    'card_integration_id': options.type === 'cash' ? config.integration[config.status]['kiosk'] : config.integration[config.status]['card'],
+                    'integration_id': config.integration[config.status][options.type],
+                    'kiosk_integration_id': config.integration[config.status][options.type],
+                    'card_integration_id': config.integration[config.status][options.type],
+                    'wallet_integration_id': config.integration[config.status][options.type],
                     'order_id': orderId,
                     'billing_data': {
                         'first_name': options.firstName,
                         'last_name': options.lastName,
                         'phone_number': options.code + options.mobile,
                         'email': options.email,
-                        'apartment': 'NA',
-                        'floor': 'NA',
-                        'street': 'NA',
-                        'building': 'NA',
-                        'shipping_method': 'NA',
-                        'postal_code': 'NA',
-                        'city': 'NA',
                         'country': options.country,
-                        'state': 'NA'
+                        'apartment': options.address.apartment || 'NA',
+                        'floor': options.address.floor || 'NA',
+                        'street': options.address.street || 'NA',
+                        'building': options.address.building || 'NA',
+                        'shipping_method': options.address.shipping_method || 'NA',
+                        'postal_code': options.address.postal_code || 'NA',
+                        'city': options.address.city || 'NA',
+                        'state': options.address.state || 'NA'
                     }
                 },
             });
-            console.log('generatePaymentToken... response --> ', response);
-            if (response.error) console.log('error generating payment', response.error);
-            console.log('body', response.data)
+            if (response.error) {
+                console.warn('error in generating payment keys error', response.error);
+                throw response.error;
+            }
             paymentToken = response.data.token;
         } catch (e) {
-            console.log('generatePaymentToken error', e)
+            console.warn('error in generating payment keys error', e)
+            throw e;
         }
     }
 
     async createURL(paymentToken) {
-        return `${baseUrl}/acceptance/iframes/${config.iFrameId}?payment_token=${paymentToken}`  //27874
+        return `${baseUrl}/acceptance/iframes/${config.iFrameId}?payment_token=${paymentToken}`;
     }
 
-    async createReference(paymentToken) {
-        console.log('createReference req', paymentToken);
-        try {
-            const response = await axios({
-                method: 'post',
-                url: `${baseUrl}/acceptance/payments/payment_token=${paymentToken}`,
-                headers: { 'content-type': 'application/json' },
-                data: {
-                    'source': {
-                        'identifier': 'AGGREGATOR',
-                        'subtype': 'AGGREGATOR'
-                    },
-                    'payment_token': paymentToken
-                }
-            });
-            console.log('createReference response', response);
-            return response.data.data ? response.data.data.bill_reference : null;
-        } catch (e) {
-            console.log('createReference error', e)
-        }
-    }
+    // create reference for cash payment
+    // async createReference(paymentToken) {
+    //     try {
+    //         const response = await axios({
+    //             method: 'post',
+    //             url: `${baseUrl}/acceptance/payments`,
+    //             headers: { 'content-type': 'application/json' },
+    //             params: {
+    //                 payment_token: paymentToken,
+    //             },
+    //             data: {
+    //                 'source': {
+    //                     'identifier': 'AGGREGATOR',
+    //                     'subtype': 'AGGREGATOR'
+    //                 },
+    //                 'payment_token': paymentToken
+    //             }
+    //         });
+    //         return response.data.data ? response.data.data.bill_reference : null;
+    //     } catch (e) {
+    //         console.warn('createReference error', e)
+    // throw e;
+    //     }
+    // }
+
     // set options to generate iFrame url.
     setOptions(props) {
-        console.log('setOptions called');
         options = {
-            type: (props.type ? 'cash' : 'card'),
+            type: props.type || 'card',
             uniqueId: props.uniqueId,
             currency: props.currency,
             price: Number((props.price).toFixed(0)),
@@ -150,31 +165,39 @@ class AcceptService {
             firstName: props.firstName,
             lastName: props.lastName,
         };
-        console.log('options', options);
+        if (props.address) {
+            options.address = props.address
+        } else {
+            options.address = {};
+        }
     }
 
     async getIframeUrl(props) {
-        this.setOptions(props);
-        console.log('optionsoptionsoptionsoptions', options);
-        if (!options || !options.uniqueId || !options.price) {
-            return {
-                status: 400,
-                message: 'bad request'
-            };
-        }
-        await this.getToken();
-        const { orderId } = await this.getOrder();
-        if (isNaN(orderId)) return { message: 'orderId from accept is not a number' };
-        await this.generatePaymentToken(orderId);
-
-        if (paymentToken) {
-            if (options.type === 'cash') {
-                return await this.createReference(paymentToken);
-            } else {
-                const url = await this.createURL(paymentToken);
-                console.log('url', url);
-                return url;
+        try {
+            this.setOptions(props);
+            if (!options || !options.uniqueId || !options.price) {
+                return {
+                    status: 400,
+                    message: 'bad request'
+                };
             }
+            await this.getToken();
+            const { orderId } = await this.getOrder();
+            if (isNaN(orderId)) return { message: 'orderId from accept is not a number' };
+            await this.generatePaymentToken(orderId);
+
+            if (paymentToken) {
+                const url = await this.createURL(paymentToken);
+                return url;
+                // if (options.type === 'cash') {
+                //     return await this.createReference(paymentToken);
+                // } else {
+                //     const url = await this.createURL(paymentToken);
+                //     return url;
+                // }
+            }
+        } catch (e) {
+            throw e;
         }
     };
 }
